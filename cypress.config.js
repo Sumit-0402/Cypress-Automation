f4e35dea1f902c7d60b8988b1696edb6517eaf51
SumitV <sumit.verma@centilytics.com>
2025-05-13 17:55:27 +0530

    changesinnode

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("Cypress");
+
+module.exports = defineConfig({
+
+
+
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
