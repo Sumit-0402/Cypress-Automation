75909074f1e8b1727e6ee2dd7e511db8a548109a
SumitV <sumit.verma@centilytics.com>
2025-05-21 12:39:55 +0530

    Initial Commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,4 @@
-const { defineConfig } = require("Cypress");
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
@@ -6,6 +6,7 @@ module.exports = defineConfig({
 
   chromeWebSecurity: false,
   e2e: {
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
